8f9de02977342ab815aa5805d577654de0c80cc4
Andrew Reitz <aj.reitz@gmail.com>
2014-05-08 08:14:15 -0500

    working on nav drawer

--- src/main/java/co/nodeath/magichealthcounter/ui/MainActivity.java
+++ src/main/java/co/nodeath/magichealthcounter/ui/MainActivity.java
@@ -1,29 +1,38 @@
 package co.nodeath.magichealthcounter.ui;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-
-import co.nodeath.magichealthcounter.R;
-import co.nodeath.magichealthcounter.MagicLifeCounterApp;
-import co.nodeath.magichealthcounter.ui.AppContainer;
-import co.nodeath.magichealthcounter.ui.misc.BaseActivity;
 
 import javax.inject.Inject;
 
+import butterknife.ButterKnife;
+import co.nodeath.magichealthcounter.MagicLifeCounterApp;
+import co.nodeath.magichealthcounter.R;
+import co.nodeath.magichealthcounter.ui.misc.BaseActivity;
+
+import static butterknife.ButterKnife.findById;
+
 public class MainActivity extends BaseActivity {
 
-    @Inject AppContainer appContainer;
+  @Inject AppContainer appContainer;
 
-    private ViewGroup container;
+  private ViewGroup container;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        MagicLifeCounterApp app = MagicLifeCounterApp.get(this);
+    MagicLifeCounterApp app = MagicLifeCounterApp.get(this);
 
-        container = appContainer.get(this, app);
+    container = appContainer.get(this, app);
 
-        getLayoutInflater().inflate(R.layout.activity_main, container);
-    }
+    getLayoutInflater().inflate(R.layout.activity_main, container);
+
+    ViewGroup drawer = findById(this, R.id.navigation_drawer);
+    LayoutInflater.from(this).inflate(R.layout.drawer_layout, drawer);
+
+    ButterKnife.inject(this);
+  }
 }
