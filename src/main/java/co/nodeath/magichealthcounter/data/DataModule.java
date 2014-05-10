de74df1ad6548f4ec37519c45e20ea566f823531
Andrew Reitz <aj.reitz@gmail.com>
2014-05-09 22:46:43 -0500

    Implemented Nav Drawer

--- src/main/java/co/nodeath/magichealthcounter/data/DataModule.java
+++ src/main/java/co/nodeath/magichealthcounter/data/DataModule.java
@@ -4,19 +4,20 @@ import android.app.Application;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import co.nodeath.magichealthcounter.ForApplication;
-
 import javax.inject.Singleton;
 
+import co.nodeath.magichealthcounter.ForApplication;
 import dagger.Module;
 import dagger.Provides;
 
 @Module(
-        complete = false,
-        library = true
+    complete = false,
+    library = true
 )
 public final class DataModule {
-    @Provides @Singleton SharedPreferences provideSharedPreferences(@ForApplication Application app) {
-        return PreferenceManager.getDefaultSharedPreferences(app);
-    }
+  @Provides @Singleton SharedPreferences provideSharedPreferences(@ForApplication Application app) {
+    return PreferenceManager.getDefaultSharedPreferences(app);
+  }
+
+
 }
--- src/main/java/co/nodeath/magichealthcounter/ui/MainActivity.java
+++ src/main/java/co/nodeath/magichealthcounter/ui/MainActivity.java
@@ -1,13 +1,21 @@
 package co.nodeath.magichealthcounter.ui;
 
+import android.app.ActionBar;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import co.nodeath.magichealthcounter.MagicLifeCounterApp;
 import co.nodeath.magichealthcounter.R;
 import co.nodeath.magichealthcounter.ui.misc.BaseActivity;
@@ -18,7 +26,16 @@ public class MainActivity extends BaseActivity {
 
   @Inject AppContainer appContainer;
 
+  @InjectView(R.id.nav_drawer_layout) DrawerLayout drawerLayout;
+  @InjectView(R.id.navigation_drawer) ViewGroup navdrawerContainer;
+  @InjectView(R.id.nav_drawer_about) TextView about;
+  @InjectView(R.id.nav_drawer_casual) TextView casual;
+  @InjectView(R.id.nav_drawer_multi_player) TextView multiplayer;
+  @InjectView(R.id.nav_drawer_tournament) TextView tournament;
+
+  private ActionBarDrawerToggle drawerToggle;
   private ViewGroup container;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +49,63 @@ public class MainActivity extends BaseActivity {
 
     ViewGroup drawer = findById(this, R.id.navigation_drawer);
     LayoutInflater.from(this).inflate(R.layout.drawer_layout, drawer);
-
     ButterKnife.inject(this);
+
+    final ActionBar actionBar = getActionBar();
+    if (actionBar == null) return; // should never happen but shuts lint up
+
+    drawerToggle = new ActionBarDrawerToggle(
+        this,
+        drawerLayout,
+        R.drawable.ic_drawer,
+        R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close
+    ) {
+
+      /** Called when a drawer has settled in a completely closed state. */
+      public void onDrawerClosed(View view) {
+        super.onDrawerClosed(view);
+        actionBar.setTitle(getString(R.string.app_name));
+        invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+      }
+
+      /** Called when a drawer has settled in a completely open state. */
+      public void onDrawerOpened(View drawerView) {
+        super.onDrawerOpened(drawerView);
+        actionBar.setTitle("TEST2");
+        invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+      }
+    };
+
+    drawerLayout.setDrawerListener(drawerToggle);
+    drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
+
+    actionBar.setDisplayHomeAsUpEnabled(true);
+    actionBar.setHomeButtonEnabled(true);
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    drawerToggle.onConfigurationChanged(newConfig);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Pass the event to ActionBarDrawerToggle, if it returns
+    // true, then it has handled the app icon touch event
+    if (drawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    // Handle your other action bar items...
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    // Sync the toggle state after onRestoreInstanceState has occurred.
+    drawerToggle.syncState();
   }
 }
