670e4045ff4b2ac245160261f392aa5ec863fc78
Andrew Reitz <aj.reitz@gmail.com>
2014-08-23 11:05:45 -0500

    Fixed issue where poison counters where showing in landscape

--- src/main/java/co/nodeath/magichealthcounter/ui/misc/BaseFragment.java
+++ src/main/java/co/nodeath/magichealthcounter/ui/misc/BaseFragment.java
@@ -4,16 +4,19 @@ import android.app.Fragment;
 import android.os.Bundle;
 
 import butterknife.ButterKnife;
+import hugo.weaving.DebugLog;
 import icepick.Icepick;
 
 public abstract class BaseFragment extends Fragment {
 
+  @DebugLog
   @Override public void onActivityCreated(Bundle savedInstanceState) {
     super.onActivityCreated(savedInstanceState);
     // Restore objects saved with Icepick
     Icepick.restoreInstanceState(this, savedInstanceState);
   }
 
+  @DebugLog
   @Override public void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
     // Save out objects annotated with @Icicle
